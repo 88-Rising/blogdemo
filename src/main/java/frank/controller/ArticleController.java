03ad247c2e71ccff519ab7f3629a8400d5b0ab13
88-Rising <348514635@qq.com>
2020-05-24 22:59:32 +0800

    增加写作页面

--- src/main/java/frank/controller/ArticleController.java
+++ src/main/java/frank/controller/ArticleController.java
@@ -46,7 +46,6 @@ public class ArticleController {
     }
 
     @RequestMapping("/writer")//页面需要什么属性就注入什么属性
-
     public String writer(HttpSession session,Model model){//根据session获取到User 返回文章列表
         User user=(User) session.getAttribute("user");
 //        List<Article> articles=articleService.
