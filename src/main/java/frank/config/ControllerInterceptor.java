8e4014143b444f2dcf8128e7823028bb4dde9270
88-Rising <348514635@qq.com>
2020-06-06 16:17:48 +0800

    增加报错页面，如果出现错误直接反应错误信息在页面

---
+++ src/main/java/frank/config/ControllerInterceptor.java
@@ -0,0 +1,25 @@
+package frank.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+@Controller
+public class ControllerInterceptor {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handle(Exception e){//如果报错的话 直接把报错信息返回到页面
+        StringWriter sw=new StringWriter();
+        PrintWriter pw=new PrintWriter(sw);
+        e.printStackTrace(pw);
+        ModelAndView mv=new ModelAndView();
+        mv.addObject("message",e.getMessage());//异常信息
+        mv.addObject("stackTrace",sw.toString());//错误的堆栈信息
+        mv.setViewName("error");
+        return mv;
+
+    }
+}
