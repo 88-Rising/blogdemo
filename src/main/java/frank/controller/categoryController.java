84b1747c5ca28af63f39be66b20a8150dc9eef7c
88-Rising <348514635@qq.com>
2020-06-07 09:57:38 +0800

    增加新建文章分类功能并且把新建的文章类名放到文章分类列表中

--- src/main/java/frank/controller/categoryController.java
+++ src/main/java/frank/controller/categoryController.java
@@ -1,6 +1,7 @@
 package frank.controller;
 
 import frank.model.Category;
+
 import frank.model.User;
 import frank.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +12,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpSession;
 
 @Controller
-public class categoryController {//增加发布文章功能
+public class categoryController {//增加文章分类功能
 
     @Autowired
     private CategoryService categoryService;//增加文章类属性
 
-    @RequestMapping(value="/c/add",method = RequestMethod.POST)//发布文章功能
+    @RequestMapping(value="/c/add",method = RequestMethod.POST)//文章分类功能
     public String addCategory(HttpSession session, Category category){//userId从session里边取
         User user = (User)session.getAttribute("user");
-        category.setId(user.getId());
-        int num=categoryService.insert(category);//把新生成的文章放入文章列表
+        category.setUserId(user.getId());
+        int num=categoryService.insert(category);//把新生成的文章分类名放入文章分类列表
         return "redirect:/writer";
     }
 
